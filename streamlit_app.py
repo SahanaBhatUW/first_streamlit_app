b64904e5c110fdf09ac2efd6ebfb6b1be1223a4a
SahanaBhatUW <112519048+SahanaBhatUW@users.noreply.github.com>
2022-08-30 15:01:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('Hello! This is Sahana')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
